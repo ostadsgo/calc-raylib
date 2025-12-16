8a28fb7ee9a137d9147757c4ee63a253fefeedff
Saeed Gholami <saeed.ghollami@gmail.com>
2025-12-16 23:09:01 +0330

    Create rectangle to become as number input text.

--- app.c
+++ app.c
@@ -1,7 +1,38 @@
 
 #include <stdio.h>
+#include "raylib.h"
 
 int main() {
-    printf("Hello World!\n");
+    // constants
+    const int SCREEN_WIDTH = 450;
+    const int SCREEN_HEIGHT = 550;
+    const char *WINDOW_TITLE = "Calculator";
+    // const int MAX_INPUT_CHARS = 30;
+    const int FPS = 60;
+
+    // array to store typed letters
+    // char inputText[MAX_INPUT_CHARS + 1] = "\0";
+    // int letterCount = 0;
+    // bool mouseOnText = false;
+
+    // x, y, w, h
+    // for width: 10 for x 10 for space form right
+    Rectangle textBox = {10, 10, SCREEN_WIDTH - 20, 50 };
+
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+    SetTargetFPS(FPS);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        
+        ClearBackground(RAYWHITE);
+        DrawRectangleRec(textBox, LIGHTGRAY);
+        DrawRectangleLinesEx(textBox, 2, DARKGRAY);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
