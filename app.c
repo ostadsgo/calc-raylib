8b6225811ccde0694f216a5c844485b8f7f08a4d
Saeed Gholami <saeed.ghollami@gmail.com>
2025-12-17 05:12:10 +0330

    Type whatever user typed in a input box.

--- app.c
+++ app.c
@@ -1,53 +1,73 @@
-
 #include <stdio.h>
 #include "raylib.h"
 
-int main() {
-    // constants
-    const int SCREEN_WIDTH = 450;
-    const int SCREEN_HEIGHT = 550;
-    const char *WINDOW_TITLE = "Calculator";
-    const int MAX_INPUT_CHARS = 30;
-    const int FPS = 60;
+// constants
+const int SCREEN_WIDTH = 450;
+const int SCREEN_HEIGHT = 550;
+const char *WINDOW_TITLE = "Calculator";
+const int MAX_INPUT_CHARS = 30;
+const int FPS = 60;
+
+
+
+void store_number(char input_text[], int key, int *letter_count)
+{
+    if (*letter_count < MAX_INPUT_CHARS) {
+        // store char and update end of char
+        input_text[*letter_count] = (char)key;
+        input_text[*letter_count + 1] = '\0';
+        (*letter_count)++;
+    }
+}
+
+void backspace_pressed(char input_text[], int *letter_count) 
+{
+    if (IsKeyDown(KEY_BACKSPACE)) {
+        (*letter_count)--;
+        if (*letter_count < 0) 
+            *letter_count = 0;
+        input_text[*letter_count] = '\0';
+    }
+}
+
+void get_key(char input_text[], int *letter_count) 
+{
+    int key = GetCharPressed();
+
+    while (key > 0) {
+        store_number(input_text, key, &letter_count);
+        key = GetCharPressed();
+    }
+
+}
+
+int main(void) 
+{
 
     // array to store typed letters
-    char inputText[MAX_INPUT_CHARS + 1];
-    inputText[0] = '\0';
-    int letterCount = 0;
+    char input_text[MAX_INPUT_CHARS + 1];
+    input_text[0] = '\0';
+    int letter_count = 0;
     // bool mouseOnText = false;
 
     // x, y, w, h
     // for width: 10 for x 10 for space form right
-    Rectangle textBox = {10, 10, SCREEN_WIDTH - 20, 50 };
-
+    Rectangle text_box = {10, 10, SCREEN_WIDTH - 20, 50 };
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(FPS);
 
     while (!WindowShouldClose()) {
-        int key = GetCharPressed();
-
-        while (key > 0) {
-            printf("The key pressed: %c", key);
-            if (letterCount < MAX_INPUT_CHARS) {
-                // store char and update end of char
-                inputText[letterCount] = (char)key;
-                inputText[letterCount + 1] = '\0';
-                letterCount++;
-            }
-            // get next char
-            key = GetCharPressed();
-        }
-
-        if (IsKeyPressed(KEY_BACKSPACE)) {
-            letterCount--;
-        }
+        get_key(input_text, &letter_count);
+        backspace_pressed(input_text, &letter_count);
 
         BeginDrawing();
-        
+
         ClearBackground(RAYWHITE);
-        DrawRectangleRec(textBox, LIGHTGRAY);
-        DrawRectangleLinesEx(textBox, 2, DARKGRAY);
+        DrawRectangleRec(text_box, LIGHTGRAY);
+        DrawRectangleLinesEx(text_box, 2, DARKGRAY);
+
+        DrawText(input_text, (int)text_box.x + 5, (int)text_box.y + 10, 30, MAROON);
 
         EndDrawing();
     }
@@ -55,3 +75,4 @@ int main() {
     CloseWindow();
     return 0;
 }
+
